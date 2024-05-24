123371727c001b44d27ddfc4a4c1ddeab85573c3
tenebrissilvam <66372087+tenebrissilvam@users.noreply.github.com>
2024-05-24 18:33:38 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
+from scipy import stats
 
+chat_id = 227118805 # Ваш chat ID, не меняйте название переменной
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+def solution(control: np.array, test: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    t_stat, p_val = stats.ttest_ind(control, test)
+    return p_val < 0.01 # Ваш ответ, True или False
